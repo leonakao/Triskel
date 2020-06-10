459ee9c69fe1e8215268638fddaf681884be0d98
Leonardo Nakao <leonakao.dev@gmail.com>
2020-06-10 22:12:52 +0000

    Add auto-fix

--- nuxt.config.js
+++ nuxt.config.js
@@ -63,13 +63,16 @@ export default {
     ** You can extend webpack config here
     */
         extend (config, ctx) {
-
+            // Fix eslint erros on save
             if (ctx.isDev && ctx.isClient) {
                 config.module.rules.push({
                     enforce: "pre",
                     test: /\.(js|vue)$/,
                     loader: "eslint-loader",
-                    exclude: /(node_modules)/
+                    exclude: /(node_modules)/,
+                    options: {
+                        fix: true,
+                    },
                 });
             }
         }
