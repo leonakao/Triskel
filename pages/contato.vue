708407194382f62087f43665279d4b6874592e6e
AchedWolf <vini.puchille@gmail.com>
2020-06-11 01:45:40 -0300

    .

--- pages/contato.vue
+++ pages/contato.vue
@@ -49,6 +49,7 @@
             </v-row>
         </v-container>
 
+        <!-- Mapa -->
         <v-container>
             <iframe
                 src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3658.6894775971746!2d-46.282636584878766!3d-23.507691665497955!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce79843d77f147%3A0x3234e2dca8bb2c74!2sEstr.%20Port%C3%A3o%20do%20Ronda%2C%203305%20-%20Jardim%20Europa%2C%20Suzano%20-%20SP%2C%2008696-043!5e0!3m2!1spt-BR!2sbr!4v1591809127611!5m2!1spt-BR!2sbr"
